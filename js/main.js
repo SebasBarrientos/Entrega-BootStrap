ad0ec705ef1cdaa5132ef431f9ade9142bc7e029
BrunoMalfi <malfi.bruno@gmail.com>
2024-04-07 21:52:47 +0200

    Badges and  spinner change

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ console.log('---------main.js--------')
 let pikachubtn = document.getElementById("evolucionaPikachu");
 let pikachucard = document.getElementById("picachucard");
 let pikachuCardImg = pikachucard.childNodes[1]
-console.log('picachubtn : ', pikachubtn)
-console.log('pikachucard : ', pikachucard)
-console.log('pikachuCardImg : ', pikachuCardImg)
+let spinnerBtn =document.getElementById("newSpinner");
+let spinnerDiv =document.getElementById("spinnerInModal");
+let clickCounter =document.getElementById("clickCounter");
+let resetClickCounter =document.getElementById("resetClickCounter");
+let clickCounterNumber = 0; 
+
+const spinnerTypeArray =['primary','secondary','success','danger','warning','info','light','dark'];
+// console.log('picachubtn : ', pikachubtn)
+// console.log('pikachucard : ', pikachucard)
+ //console.log('pikachuCardImg : ', pikachuCardImg)
 
 
 
@@ -18,5 +25,28 @@ function changeImg(event){
     }
     console.log('pikachuCardImg2 : ', pikachuCardImg)    
 }
+function changeSpinner(event){
+    const newSpinner = "spinner-border text-"+spinnerTypeArray[Math.floor(Math.random() * spinnerTypeArray.length)]
+    spinnerDiv.setAttribute("class",newSpinner)
+}
+function clickCounterF (event){
+    clickCounterNumber++;
+    event.target.childNodes[1].innerHTML=clickCounterNumber;
+}
+function clickCounterF (event){
+    clickCounterNumber++;
+    event.target.childNodes[1].innerHTML=clickCounterNumber;
+    resetClickCounter.childNodes[1].setAttribute("class","position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle")
+}
+
+function resetClickCounterF (event){
+    clickCounterNumber = 0;
+    clickCounter.childNodes[1].innerHTML=clickCounterNumber;
+    resetClickCounter.childNodes[1].setAttribute("class","")
+    
+}
 
 pikachubtn.addEventListener('click',changeImg)
+spinnerBtn.addEventListener('click',changeSpinner)
+clickCounter.addEventListener('click',clickCounterF)
+resetClickCounter.addEventListener('click',resetClickCounterF)
